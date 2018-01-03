e02964cfec1cd8b83ff77ae556e7e7b98d8bbe4f
Maganty Rushyendra <mrushyendra@yahoo.com.sg>
2018-01-03 15:43:57 +0800

    Update denseFlow.h

--- denseFlow.h
+++ denseFlow.h
@@ -1,6 +1,11 @@
 #ifndef DENSEFLOW_H
 #define DENSEFLOW_H
 
+#include "opencv2/opencv.hpp"
+#include "opencv2/ccalib/omnidir.hpp"
+#include "opencv2/xfeatures2d.hpp"
+#include "opencv2/optflow.hpp"
+
 double calcMedian(Mat data, Mat exclude, Mat mask);
 int denseFlow(char* videoFileName);
 
