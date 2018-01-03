2c6dd329e05313c6db2142c6d12b5f02ad78eb3b
Maganty Rushyendra <mrushyendra@yahoo.com.sg>
2018-01-03 15:41:52 +0800

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,6 +1,20 @@
 
 #include sparseFlow.h
 #include denseFlow.h
+#include "opencv2/opencv.hpp"
+#include "opencv2/ccalib/omnidir.hpp"
+#include "opencv2/xfeatures2d.hpp"
+#include "opencv2/optflow.hpp"
+#include <cstdlib>
+#include <cstdio>
+#include <cmath>
+#include <iostream>
+#include <stdexcept>
+#include <algorithm>
+
+using namespace std;
+using namespace cv;
+using namespace cv::ml;
 
 int main(int argc, char** argv) {
 	denseFlow(argv[1]);
