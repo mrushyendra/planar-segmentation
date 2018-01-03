76c30c63c07b80491f74bfdabfc01ea1316caf4d
Maganty Rushyendra <mrushyendra@yahoo.com.sg>
2018-01-03 15:44:44 +0800

    Update sparseFlow.h

--- sparseFlow.h
+++ sparseFlow.h
@@ -1,6 +1,11 @@
 #ifndef SPARSEFLOW_H
 #define SPARSEFLOW_H
 
+#include "opencv2/opencv.hpp"
+#include "opencv2/ccalib/omnidir.hpp"
+#include "opencv2/xfeatures2d.hpp"
+#include "opencv2/optflow.hpp"
+
 float euclideanDist(Point2f& p, Point2f& q);
 int sparseFlow(char* videoFileName);
 
